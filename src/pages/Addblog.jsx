918b8bbbda0c9ca2bf4cddf6607229ac7229a4ea
Ppriyanka585 <ppriyankababu1@gmail.com>
2023-05-28 18:35:05 +0530

    firstcommit

--- src/pages/Addblog.jsx
+++ src/pages/Addblog.jsx
@@ -1,43 +1,33 @@
 import React from 'react'
-import { useForm } from 'react-hook-form';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
-export default function App() {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data); 
+export default function AddBlog() {
+  
+  const handleClick =(e)=> {
   };
   return (
-
-     <form onSubmit={handleSubmit(onSubmit)}>
-      <h1 className='text-center mt-5'>FORM</h1>
-      <div className='text-center mt-5'>
-        <label htmlFor="blogName">Blog Name:</label>
-        <input
-          type="text"
-          id="blogName"
-          {...register('blogName', { required: true })}
-        />
-      </div>
-      <div className='text-center mt-5'>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          {...register('description', { required: true })}
-        ></textarea>
-      </div>
-      <div className='text-center mt-5'>
-        <label htmlFor="authorName">Author Name:</label>
-        <input
-          type="text"
-          id="authorName"
-          {...register('authorName', { required: true })}
-        />
-      </div>
-      <button variant="primary" type ="submit" onclick={handleClick}>Submit</button>{' '}
-    </form>
-   
-);
+    <div>
+      <h1 className='text-center mt-5'>ADD BLOG</h1>
+      <Container>
+      <Form onSubmit={handleClick}>
+        <Form.Group className="mb-3" controlId=" ">
+          <Form.Label>Blog Name</Form.Label>
+          <Form.Control type="text" placeholder=" " />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Label>Description</Form.Label>
+          <Form.Control type="text" placeholder=" " />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId=" ">
+          <Form.Label>Author Name</Form.Label>
+          <Form.Control type="text" placeholder=" " />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleClick} >Submit</Button>{' '}
+      </Form>
+      
+      </Container>
+    </div>
+  )
 }
-
-  
